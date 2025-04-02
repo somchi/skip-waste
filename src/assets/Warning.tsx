f9e80549117436718c86780ed9603f11be1dfb24
somchi <chisomike11@gmail.com>
2025-04-02 16:19:45 +0100

    consumed data

---
+++ src/assets/Warning.tsx
@@ -0,0 +1,20 @@
+export const WarningIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="3"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      className="lucide lucide-alert-triangle w-3 h-3 text-yellow-500 shrink-0"
+    >
+      <path d="m21.73 18-8-14a2 2 0 0 0-3.48 0l-8 14A2 2 0 0 0 4 21h16a2 2 0 0 0 1.73-3Z"></path>
+      <path d="M12 9v4"></path>
+      <path d="M12 17h.01"></path>
+    </svg>
+  );
+};
--- src/components/SkipCard.tsx
+++ src/components/SkipCard.tsx
@@ -3,6 +3,8 @@ import { Skip } from '../utils/types';
 import { Checkbox } from './Checkbox';
 import { AppContext } from '../context/app.context';
 import { SET_SELECTED_SKIP } from '../context/reducers/app.reducer';
+import { WarningIcon } from '../assets/Warning';
+import { DangerIcon } from '../assets/Danger';
 
 type Props = {
   skipItem: Skip;
@@ -15,18 +17,18 @@ export default function SkipCard({ skipItem }: Props) {
     const active = state.selected.id === skipItem.id ? ({} as Skip) : skipItem;
     dispatch({ type: SET_SELECTED_SKIP, payload: { selected: active } });
   };
-  console.log(state);
+
   return (
     <div
-      className={`flex items-center bg-gray-gray-800 border rounded-lg 
+      className={`flex items-center bg-gray-gray-800 border rounded-lg relative
       shadow-sm md:flex-row w-full md:w-[390px] hover:bg-gray-700 bg-gray-800 h-[250px]  ${
         state.selected.id && state.selected.id === skipItem.id
           ? 'border-blue-500 border-2'
           : 'border-gray-700'
       } `}
     >
-      <div className="flex rounded-lg w-1/3 h-full bg-[url('./image.png')] relative">
-        <div className="z-50 flex w-full p-4">
+      <div className="flex flex-col px-2 py-4 justify-between rounded-lg w-1/2 h-full bg-[url('./image.png')] relative">
+        <div className="z-50 flex w-full">
           <Checkbox
             checked={
               state.selected.id && state.selected.id === skipItem.id
@@ -36,24 +38,51 @@ export default function SkipCard({ skipItem }: Props) {
             handleChange={handleClick}
           />
         </div>
+        <div className="flex flex-col gap-2 z-10">
+          {!skipItem.allowed_on_road && (
+            <div className="flex gap-x-1 bg-black rounded-full items-center p-[5px]">
+              <WarningIcon />
+              <span className="text-xs text-start text-yellow-500 font-semibold flex mt-[-2px]">
+                Private property only
+              </span>
+            </div>
+          )}
+          {!skipItem.allows_heavy_waste && (
+            <div className="flex gap-x-1 bg-black items-center rounded-full p-[3px]">
+              <DangerIcon />
+              <span className="text-xs text-start text-red-500 font-semibold mt-[-2px] flex">
+                Not suitable for heavy waste
+              </span>
+            </div>
+          )}
+        </div>
         <div className="absolute inset-0 bg-black bg-opacity-20 pointer-events-none transition-opacity"></div>
       </div>
 
       <div className="flex flex-col justify-between p-3 md:p-4 leading-normal w-2/3 gap-y-12">
         <div className="flex flex-col">
           <div className="flex justify-between">
-            <h1 className="text-xl font-bold">6 Yard Skip</h1>
+            <h1 className="text-md md:text-xl font-bold">
+              {skipItem.size} Yard Skip
+            </h1>
             <div className="flex items-center bg-blue-500 rounded-full px-2 py-0">
-              <span className="text-xs font-semibold">6 Yards</span>
+              <span className="text-xs font-semibold">
+                {skipItem.size} {skipItem.size > 1 ? 'Yards' : 'Yard'}
+              </span>
             </div>
           </div>
           <div className="grid justify-start">
-            <span className="text-gray-400">14 days hire period</span>
+            <span className="text-gray-400">
+              {skipItem.hire_period_days}{' '}
+              {skipItem.hire_period_days > 1 ? 'days' : 'day'} hire period
+            </span>
           </div>
         </div>
         <div className="flex flex-col gap-4 justify-start">
           <p className="text-start">
-            <strong className="text-blue-500 font-bold text-2xl">£269</strong>
+            <strong className="text-blue-500 font-bold text-2xl">
+              £{skipItem.price_before_vat}
+            </strong>
             <span className="text-sm text-gray-400"> / per week</span>
           </p>
           <button
@@ -70,6 +99,12 @@ export default function SkipCard({ skipItem }: Props) {
           </button>
         </div>
       </div>
+      {!skipItem.allows_heavy_waste && (
+        <div
+          className="absolute inset-0 rounded-lg hover:bg-transparent bg-black 
+        bg-opacity-45 pointer-events-none transition-opacity"
+        ></div>
+      )}
     </div>
   );
 }
